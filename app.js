0d55bc1fda4ea12060ab039d981462783b988cb8
DAVID HEYDUCK <heyduckd@gmail.com>
2016-02-01 19:19:01 -0800

    re did structure because I over did it at first

---
+++ app.js
@@ -0,0 +1,34 @@
+var projects = [];
+
+function Project (opts) {
+ this.title = opts.title;
+ this.gitRepo = opts.gitRepo;
+ this.url = opts.url;
+ this.body = opts.body;
+};
+
+Project.prototype.toHtml = function () {
+ var $newProject = $('article.template').clone();
+ $newProject.removeClass('template');
+
+ $newProject.find('header > h1').text(this.title);
+
+ $newProject.find('#ghRepo').html('<a href= "' + this.gitRepo + '">GitHub Repo</a>');
+
+ $newProject.find('#website').html('<a href= "' + this.url + '">Project Link</a>');
+
+ $newProject.find('.site-body').html(this.body);
+
+ $newProject.append('<hr>');
+
+ return $newProject;
+
+};
+
+rawProject.forEach(function(ele) {
+ projects.push(new Project(ele));
+});
+
+projects.forEach(function(a) {
+ $('#projects').append(a.toHtml());
+});
