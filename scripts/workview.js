f4d81539ca4df21f5d21a8525131bf15607781e5
DAVID HEYDUCK <heyduckd@gmail.com>
2016-02-15 18:00:03 -0800

    client side routing working

--- scripts/workview.js
+++ scripts/workview.js
@@ -1,62 +1,37 @@
 // PROJECTS
 var workView = {};
 
-workView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).show();
-  });
-  $('min-nav .tab:first').click();
-};
-
 workView.initIndexPage = function() {
   Project.all.forEach(function(a) {
     $('#work').append(a.toHtml())
   });
-  workView.handleMainNav();
 };
 
 
 // EDUCATION
 var eduView = {};
 
-eduView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).show();
-  });
-  $('min-nav .tab:first').click();
-};
-
 eduView.initIndexPage = function() {
+  $('#education').empty();
   Edu.all.forEach(function(a) {
     $('#education').append(a.toHtml())
   });
-  eduView.handleMainNav();
 };
 
 
 // CONNECT
 var connectView = {};
 
-connectView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).show();
-  });
-  $('min-nav .tab:first').click();
-};
-
 connectView.initIndexPage = function() {
+  $('#connect').empty();
   Connect.all.forEach(function(a) {
     $('#connect').append(a.toHtml())
   });
-  connectView.handleMainNav();
 };
 
 // HIDES CONTENT
 $(document).ready(function() {
-  workView.handleMainNav();
+  // workView.handleMainNav();
   $('.bxslider').bxSlider();
   $('#work').hide();
   $('#education').hide();
